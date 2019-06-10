b2db006f2652c45fd0830a39e72dd90cbf0a5a89
ynFatal <63413763@qq.com>
2019-06-10 12:00:33 +0800

    Eureka 服务注册与发现

---
+++ chapter1/eureka-client/src/main/java/com/fatal/EurekaClientApplication.java
@@ -0,0 +1,28 @@
+package com.fatal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 注册中心是eureka，那么就推荐@EnableEurekaClient；
+ * 如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
+ */
+@SpringBootApplication
+// 开启发现 Eureka 客户端相关配置
+@EnableEurekaClient
+@RestController
+public class EurekaClientApplication {
+
+    @RequestMapping("/client")
+    public String client() {
+        return "this is eureka client";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
---
+++ chapter1/eureka-server/src/main/java/com/fatal/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.fatal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// 开启 Eureka 服务器相关配置
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
